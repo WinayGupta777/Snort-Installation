a164912b2d42faa9a1785629a55168baf87ff57a
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-09-07 11:06:01 +0530

    Initiated `main.sh`;

---
+++ main.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script is used to install the Snort IDS on a Debian based system.
+# It will install the following packages:
+#
+# - build-essential  
+# - openssl  libssl-dev  
+# - libdbus-1-dev  libpcap-dev  libpcap0.8-dev  pkg-config
+# - libpcre16-3  libpcre3-dev  libpcre32-3  libpcrecpp0v5
+# - libdumbnet-dev  libdumbnet1  
+# - bison  flex  libfl-dev  libfl2  libsigsegv2  m4  bison-doc  flex-doc  m4-doc
+# - zlib1g-dev  liblzma-dev  liblzma-doc
+# - gcc  libluajit-5.1-2  libluajit-5.1-common  libluajit-5.1-dev
+# - apt-file  libapt-pkg-perl 
+# - libexporter-tiny-perl  liblist-moreutils-perl  liblist-moreutils-xs-perl  libregexp-assemble-perl
+# - libntirpc-dev libntirpc3.5 liburcu8
+
